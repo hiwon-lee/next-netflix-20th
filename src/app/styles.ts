2af8d2ee95259dcf1cc15c74ef1b90a5b2192dc1
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-08 02:23:10 +0900

    💄 ui:  전역 Container 스타일 추가

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const LottieWrapper = styled.div`
   display: flex;
@@ -7,4 +7,15 @@ export const LottieWrapper = styled.div`
 
   width: 100%;
   height: 100%;
+  margin: 0 auto;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 375px;
+  height: 100svh;
+  max-height: 812px;
+  background: black;
 `;
