02941983524b7934d46983514821f7e00d6d7be2
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-09 02:33:08 +0900

    ✨ feat: LargeGradientCard추가

--- src/app/home/_components/style.ts
+++ src/app/home/_components/style.ts
@@ -5,3 +5,30 @@ export const FullCoveredImage = styled.img`
   height: 100%;
   object-fit: cover;
 `;
+
+export const Title = styled.p`
+  font-family: SF Pro Display;
+  font-size: 26.75px;
+  font-weight: 700;
+  line-height: 20px;
+  letter-spacing: -0.07339449226856232px;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: white;
+  margin: 1rem;
+  margin-top: 1.5rem;
+`;
+
+export const SubTitle = styled.p`
+  font-family: SF Pro Display;
+  font-size: 20.92px;
+  font-weight: 700;
+  line-height: 15.64px;
+  letter-spacing: -0.05740566551685333px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: white;
+  margin: 1rem;
+  margin-top: 1.5rem;
+`;
