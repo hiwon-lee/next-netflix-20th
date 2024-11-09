f5d0a23b5f6a74053954c37f2fc7bb550783d0dd
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-09 22:22:06 +0900

    💡comment:  env api 주석삭제

--- src/services/movieApi.ts
+++ src/services/movieApi.ts
@@ -1,11 +1,8 @@
 import { MovieProps } from '@/types/MovieProps';
 
 export const fetchMovies = async (): Promise<MovieProps[]> => {
-  //   const { API_KEY, API_URL } = process.env;
   const API_KEY = process.env.API_KEY;
   const API_URL = process.env.API_URL;
-  console.log(API_KEY);
-  console.log(API_URL);
 
   const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
   const response = await fetch(endpoint);
